f0bb36d42e252407255fc021918f58a8d84a728d
ygb <gbyu2021@163.com>
2026-07-05 22:49:45 +0800

    redis/: string 实战

--- redis/cmd/01/main.go
+++ redis/cmd/01/main.go
@@ -3,14 +3,14 @@ package main
 import (
 	"context"
 	"log"
-	"redis-learn/utils/redis"
+	"redis-learn/utils"
 )
 
 func main() {
 	// 连接 redis
-	res, err := redis.GetClient().Ping(context.Background()).Result()
+	res, err := utils.GetClient().Ping(context.Background()).Result()
 	if err != nil {
-		panic(err)
+		log.Panic(err)
 	}
 	log.Println(res)
 }
---
+++ redis/cmd/02/main.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"log"
+	"redis-learn/utils"
+	"time"
+
+	"github.com/redis/go-redis/v9"
+)
+
+func main() {
+	// redis string 实战：文章阅读量计数器
+	ctx := context.Background()
+
+	cnt, err := GetArticleViewCount(ctx, 1)
+	if err != nil {
+		log.Panic(err)
+	}
+	log.Println(cnt)
+
+	if err = IncrementArticleViewCount(ctx, 1); err != nil {
+		log.Panic(err)
+	}
+
+	cnt, err = GetArticleViewCount(ctx, 1)
+	if err != nil {
+		log.Panic(err)
+	}
+	log.Println(cnt)
+}
+
+func IncrementArticleViewCount(ctx context.Context, articleID int64) error {
+	// 1. 获取 redis 客户端
+	client := utils.GetClient()
+
+	// 2. 构建 key
+	key := fmt.Sprintf("article:view:%d", articleID)
+
+	// 3. 执行自增操作
+	value, err := client.Incr(ctx, key).Result()
+	if err != nil {
+		return err
+	}
+	if value == 1 {
+		// key 此前不存在，设置过期时间
+		if err = client.Expire(ctx, key, time.Minute*1).Err(); err != nil {
+			// 需要进行回退，否则会留下一个永不过期的 key
+			if _, err = client.Decr(ctx, key).Result(); err != nil {
+				return fmt.Errorf("回退失败: %v, 原始错误: %v", err, err)
+			}
+			return err
+		}
+	}
+
+	return nil
+}
+
+func GetArticleViewCount(ctx context.Context, articleID int64) (int64, error) {
+	// 1. 获取 redis 客户端
+	client := utils.GetClient()
+
+	// 2. 构建 key
+	key := fmt.Sprintf("article:view:%d", articleID)
+
+	// 3. 获取阅读量
+	value, err := client.Get(ctx, key).Int64()
+	if err != nil {
+		if errors.Is(err, redis.Nil) {
+			// key 不存在
+			return 0, nil
+		}
+		return 0, err
+	}
+
+	return value, nil
+}
